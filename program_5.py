fe3cd8a035abefb1fb8d792de1600651ca3eb5c7
SergeyKot987 <37594522+SergeyKot987@users.noreply.github.com>
2018-04-16 21:38:07 +0300

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -1,11 +1,9 @@
-from .function_5 import card_func, bank_func, date_func, cvv_func
+from .function_5 import card_func, date_func, cvv_func
 
 card_func_check = card_func()
-bank_func_check = bank_func()
 date_func_check = date_func()
 cvv_func_check = cvv_func()
 
 print(card_func_check)
-print(bank_func_check)
 print(date_func_check)
 print(cvv_func_check)
